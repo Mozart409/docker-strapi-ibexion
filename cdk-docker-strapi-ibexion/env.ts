a6aaf826d533b358eb97aeb5b3d9be52c957693d
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-04-30 00:07:23 +0200

    fix: :fire: removed cdk stack

--- cdk-docker-strapi-ibexion/env.ts
+++
@@ -1,4 +0,0 @@
-export const SES_REGION = "eu-central-1";
-export const SES_EMAIL_TO = "hey@mozart409.space";
-export const SES_EMAIL_FROM = "maderamadeus@gmail.com";
-export const PROJECT_NAME = "ibexion";
--- cdk-docker-strapi-ibexion/lambda/scrape.ts
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event: object, context: any) => {
-  console.log("SECRET_NAME 👉", process.env.SECRET_NAME);
-  console.log("SECRET_VALUE 👉", process.env.SECRET_VALUE);
-
-  const res = JSON.parse(`${process.env.SECRET_VALUE}`);
-
-  return { body: JSON.stringify({ message: "SUCCESS" }), statusCode: 200 };
-};
